37d39c0eff3c169d2af08cb2e7367e57266eb2c6
luansxz-insta <herick.faria@escola.pr.gov.br>
2026-02-23 12:35:40 +0000

    criar variaveis para pessoa e produto

---
+++ Variaveis/variavel-pessoa.js
@@ -0,0 +1,29 @@
+// , var, let, const (valores reservados da linguagem)
+// 0-9 0,7, 0-1, 0-9,A,B,C,D,E,F
+
+let nome = 'Petter'        // STRING
+let sobrenome = 'parker'   // STRING
+const cpf = '8888888888-88' // STRING
+const rg = '101212 ssp'    // STRING
+let idade = 22            // NUMBER
+let solteiro = true     // BOOLEAN
+const nota = 7 + 2      // NUMBER 
+
+console.log(nome)
+  // imprima aqui todas as variáveis acima
+console.log(sobrenome)
+console.log(cpf)
+console.log(rg)
+console.log(idade)
+console.log(solteiro)
+console.log(nota)
+
+
+
+console.log(typeof nome)
+console.log(typeof sobrenome)
+console.log(typeof cpf)
+console.log(typeof rg)
+console.log(typeof idade)
+console.log(typeof solteiro)
+console.log(typeof nota)
---
+++ Variaveis/variavel-produto.js
@@ -0,0 +1,19 @@
+let nome 
+let quantidade
+let preco
+let disponivel
+
+nome = 'whey protein'
+quantidade ='1kg'
+preco = '149,90'
+disponivel = true 
+
+console.log(nome)
+console.log(quantidade)
+console.log(preco)
+console.log(disponivel)
+
+console.log(typeof nome)
+console.log(typeof quantidade)
+console.log(typeof preco)
+console.log(typeof disponivel)
